dec59fef366f1fa70ac02cc2b023d6c5c544b678
pithydon <daniel@danielimac.local>
2016-05-26 09:23:12 -0600

    tnt and torch

--- init.lua
+++ init.lua
@@ -12,8 +12,10 @@ minetest.register_node("wireworld:mese_head", {
 	sounds = mese_def.sounds,
 	light_source = mese_def.light_source,
 	wireworld = "wireworld:mese_tail",
-	on_rightclick = function(pos)
-		minetest.set_node(pos, {name = "wireworld:mese_tail"})
+	on_punch = function(pos, node, puncher)
+		if puncher:get_wielded_item():get_name() == "default:torch" then
+			minetest.set_node(pos, {name = "wireworld:mese_tail"})
+		end
 	end,
 	after_place_node = function(pos)
 		table.insert(wireworld_nodes, pos)
@@ -28,8 +30,10 @@ minetest.register_node("wireworld:mese_tail", {
 	sounds = mese_def.sounds,
 	light_source = mese_def.light_source,
 	wireworld = "default:mese",
-	on_rightclick = function(pos)
-		minetest.set_node(pos, {name = "default:mese"})
+	on_punch = function(pos, node, puncher)
+		if puncher:get_wielded_item():get_name() == "default:torch" then
+			minetest.set_node(pos, {name = "default:mese"})
+		end
 	end,
 	after_place_node = function(pos)
 		table.insert(wireworld_nodes, pos)
@@ -39,8 +43,22 @@ minetest.register_node("wireworld:mese_tail", {
 minetest.override_item("default:mese", {
 	groups = {cracky = 1, level = 2, wireworld = 3},
 	wireworld = "wireworld:mese_head",
-	on_rightclick = function(pos)
-		minetest.set_node(pos, {name = "wireworld:mese_head"})
+	on_punch = function(pos, node, puncher)
+		if puncher:get_wielded_item():get_name() == "default:torch" then
+			minetest.set_node(pos, {name = "wireworld:mese_head"})
+		end
+	end,
+	after_place_node = function(pos)
+		table.insert(wireworld_nodes, pos)
+	end,
+})
+end
+
+if minetest.registered_nodes["tnt:tnt"] then
+minetest.override_item("tnt:tnt", {
+	groups = {dig_immediate = 2, mesecon = 2, tnt = 1, wireworld = 4},
+	on_wireworld = function(pos)
+		minetest.set_node(pos, {name = "tnt:tnt_burning"})
 	end,
 	after_place_node = function(pos)
 		table.insert(wireworld_nodes, pos)
@@ -56,7 +74,7 @@ minetest.register_globalstep(function(dtime)
 			local node = minetest.get_node(v)
 			local wireworld = minetest.get_item_group(node.name, "wireworld")
 			local meta = minetest.get_meta(v)
-			if wireworld == 3 then
+			if wireworld == 3 or wireworld == 4 then
 				local table = minetest.find_nodes_in_area({x = v.x - 1, y = v.y - 1, z = v.z - 1}, {x = v.x + 1, y = v.y + 1, z = v.z + 1}, {"group:wireworldhead"})
 				local count = 0
 				for _ in pairs(table) do count = count + 1 end
@@ -80,9 +98,13 @@ minetest.register_globalstep(function(dtime)
 		for k,v in pairs(wireworld_nodes) do
 			local node = minetest.get_node(v)
 			local node_def = minetest.registered_nodes[node.name]
+			local wireworld = minetest.get_item_group(node.name, "wireworld")
 			local meta = minetest.get_meta(v)
 			if meta:get_string("wireworldnext") == "true" then
-				if wireworld_go then minetest.set_node(v, {name = node_def.wireworld}) end
+				if wireworld_go then
+					if wireworld == 4 then node_def.on_wireworld(v)
+					else minetest.set_node(v, {name = node_def.wireworld}) end
+				end
 				meta:set_string("wireworldnext", "false")
 			end
 		end

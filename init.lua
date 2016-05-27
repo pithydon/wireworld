851ebc54199ac64d78dbb6e9858edd8aa494262e
pithydon <daniel@danielimac.local>
2016-05-27 11:03:56 -0600

    mesecons extrawires

--- init.lua
+++ init.lua
@@ -72,6 +72,89 @@ if (minetest.get_modpath("mesecons_button")) then
 	})
 end
 
+if (minetest.get_modpath("mesecons_extrawires")) then
+minetest.register_node("wireworld:mese_head_powered", {
+	description = mese_def.description.." Head",
+	tiles = {"default_mese_block.png^[colorize:blue:127"},
+	paramtype = "light",
+	groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1, wireworldhead = 1},
+	sounds = mese_def.sounds,
+	light_source = 5,
+	mesecons = {conductor = {
+		state = mesecon.state.on,
+		offstate = "wireworld:mese_head",
+		rules = mesewire_rules
+	}},
+	on_wireworld = function(pos)
+		minetest.swap_node(pos, {name = "wireworld:mese_tail_powered"})
+	end,
+	on_punch = function(pos, node, puncher)
+		if puncher:get_wielded_item():get_name() == "default:torch" then
+			minetest.swap_node(pos, {name = "wireworld:mese_tail_powered"})
+		end
+	end,
+	after_place_node = function(pos)
+		table.insert(wireworld_nodes, pos)
+	end,
+})
+
+minetest.register_node("wireworld:mese_tail_powered", {
+	description = mese_def.description.." Tail",
+	tiles = {"default_mese_block.png^[colorize:red:127"},
+	paramtype = "light",
+	groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1},
+	sounds = mese_def.sounds,
+	light_source = 5,
+	mesecons = {conductor = {
+		state = mesecon.state.on,
+		offstate = "wireworld:mese_tail",
+		rules = mesewire_rules
+	}},
+	on_wireworld = function(pos)
+		minetest.swap_node(pos, {name = "mesecons_extrawires:mese_powered"})
+	end,
+	on_punch = function(pos, node, puncher)
+		if puncher:get_wielded_item():get_name() == "default:torch" then
+			minetest.swap_node(pos, {name = "mesecons_extrawires:mese_powered"})
+		end
+	end,
+	after_place_node = function(pos)
+		table.insert(wireworld_nodes, pos)
+	end,
+})
+
+minetest.override_item("mesecons_extrawires:mese_powered", {
+	groups = {cracky = 1, level = 2, wireworld = 2},
+	on_wireworld = function(pos)
+		minetest.swap_node(pos, {name = "wireworld:mese_head_powered"})
+	end,
+	on_punch = function(pos, node, puncher)
+		if puncher:get_wielded_item():get_name() == "default:torch" then
+			minetest.swap_node(pos, {name = "wireworld:mese_head_powered"})
+		end
+	end,
+	after_place_node = function(pos)
+		table.insert(wireworld_nodes, pos)
+	end,
+})
+
+minetest.override_item("wireworld:mese_head", {
+	mesecons = {conductor = {
+		state = mesecon.state.off,
+		onstate = "wireworld:mese_head_powered",
+		rules = mesewire_rules
+	}}
+})
+
+minetest.override_item("wireworld:mese_tail", {
+	mesecons = {conductor = {
+		state = mesecon.state.off,
+		onstate = "wireworld:mese_tail_powered",
+		rules = mesewire_rules
+	}}
+})
+end
+
 if (minetest.get_modpath("tnt")) then
 minetest.override_item("tnt:tnt", {
 	groups = {dig_immediate = 2, mesecon = 2, tnt = 1, wireworld = 2},

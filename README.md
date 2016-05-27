3f47d72673ac6bd6ceddb2995768e00326fd268c
pithydon <daniel@danielimac.local>
2016-05-26 19:25:32 -0600

    api and beginning of mesecon support

--- README.md
+++ README.md
@@ -6,5 +6,46 @@ See [wikipedia] for more on wireworld.
 
 Licensed under unlicense see LICENSE.txt
 
+API:
+
+node groups
+
+* wireworld = 1
+* wireworld = 2
+* wireworldhead
+
+Group wireworld = 1 will run on_wireworld on next generation.
+
+Group wireworld = 2 will run on_wireworld on next generation if 1 or 2 wireworldhead is near by.
+
+on_wireworld is a part of the node definition.
+
+```lua
+on_wireworld = function(pos)
+```
+For your node to be used by wireworld when placed use
+
+```lua
+after_place_node = function(pos)
+  table.insert(wireworld_nodes, pos)
+end,
+```
+
+There is a lbm to catch nodes that have already been placed.
+
+example
+
+```lua
+minetest.register_node("new:node", {
+	groups = {wireworld = 2},
+	on_wireworld = function(pos)
+		minetest.swap_node(pos, {name = "new:node_head"})
+	end,
+	after_place_node = function(pos)
+		table.insert(wireworld_nodes, pos)
+	end,
+})
+```
+
 [pithydon]: <https://github.com/pithydon>
 [wikipedia]: <https://en.wikipedia.org/wiki/Wireworld>
--- init.lua
+++ init.lua
@@ -1,20 +1,21 @@
-local wireworld_nodes = {}
-local wireworld_go = true
+wireworld_nodes = {}
+wireworld_go = true
 
-if minetest.registered_nodes["default:mese"] then
 local mese_def = minetest.registered_nodes["default:mese"]
 
 minetest.register_node("wireworld:mese_head", {
 	description = mese_def.description.." Head",
 	tiles = {"default_mese_block.png^[colorize:blue:127"},
 	paramtype = "light",
-	groups = {cracky = 1, level = 2, wireworld = 1, wireworldhead = 1},
+	groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1, wireworldhead = 1},
 	sounds = mese_def.sounds,
 	light_source = mese_def.light_source,
-	wireworld = "wireworld:mese_tail",
+	on_wireworld = function(pos)
+		minetest.swap_node(pos, {name = "wireworld:mese_tail"})
+	end,
 	on_punch = function(pos, node, puncher)
 		if puncher:get_wielded_item():get_name() == "default:torch" then
-			minetest.set_node(pos, {name = "wireworld:mese_tail"})
+			minetest.swap_node(pos, {name = "wireworld:mese_tail"})
 		end
 	end,
 	after_place_node = function(pos)
@@ -26,13 +27,15 @@ minetest.register_node("wireworld:mese_tail", {
 	description = mese_def.description.." Tail",
 	tiles = {"default_mese_block.png^[colorize:red:127"},
 	paramtype = "light",
-	groups = {cracky = 1, level = 2, wireworld = 2},
+	groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1},
 	sounds = mese_def.sounds,
 	light_source = mese_def.light_source,
-	wireworld = "default:mese",
+	on_wireworld = function(pos)
+		minetest.swap_node(pos, {name = "default:mese"})
+	end,
 	on_punch = function(pos, node, puncher)
 		if puncher:get_wielded_item():get_name() == "default:torch" then
-			minetest.set_node(pos, {name = "default:mese"})
+			minetest.swap_node(pos, {name = "default:mese"})
 		end
 	end,
 	after_place_node = function(pos)
@@ -41,22 +44,37 @@ minetest.register_node("wireworld:mese_tail", {
 })
 
 minetest.override_item("default:mese", {
-	groups = {cracky = 1, level = 2, wireworld = 3},
-	wireworld = "wireworld:mese_head",
+	groups = {cracky = 1, level = 2, wireworld = 2},
+	on_wireworld = function(pos)
+		minetest.swap_node(pos, {name = "wireworld:mese_head"})
+	end,
 	on_punch = function(pos, node, puncher)
 		if puncher:get_wielded_item():get_name() == "default:torch" then
-			minetest.set_node(pos, {name = "wireworld:mese_head"})
+			minetest.swap_node(pos, {name = "wireworld:mese_head"})
 		end
 	end,
 	after_place_node = function(pos)
 		table.insert(wireworld_nodes, pos)
 	end,
 })
+
+if (minetest.get_modpath("mesecons_button")) then
+	local mesecons_button_def = minetest.registered_nodes["mesecons_button:button_off"]
+	minetest.override_item("mesecons_button:button_off", {
+		groups = {dig_immediate=2, mesecon_needs_receiver = 1, wireworld = 2},
+		on_wireworld = function(pos)
+			local node = minetest.get_node(pos)
+			mesecons_button_def.on_rightclick(pos, node)
+		end,
+		after_place_node = function(pos)
+			table.insert(wireworld_nodes, pos)
+		end,
+	})
 end
 
-if minetest.registered_nodes["tnt:tnt"] then
+if (minetest.get_modpath("tnt")) then
 minetest.override_item("tnt:tnt", {
-	groups = {dig_immediate = 2, mesecon = 2, tnt = 1, wireworld = 4},
+	groups = {dig_immediate = 2, mesecon = 2, tnt = 1, wireworld = 2},
 	on_wireworld = function(pos)
 		minetest.set_node(pos, {name = "tnt:tnt_burning"})
 	end,
@@ -74,15 +92,15 @@ minetest.register_globalstep(function(dtime)
 			local node = minetest.get_node(v)
 			local wireworld = minetest.get_item_group(node.name, "wireworld")
 			local meta = minetest.get_meta(v)
-			if wireworld == 3 or wireworld == 4 then
+			if wireworld == 1 then
+				meta:set_string("wireworldnext", "true")
+			elseif wireworld == 2 then
 				local table = minetest.find_nodes_in_area({x = v.x - 1, y = v.y - 1, z = v.z - 1}, {x = v.x + 1, y = v.y + 1, z = v.z + 1}, {"group:wireworldhead"})
 				local count = 0
 				for _ in pairs(table) do count = count + 1 end
 				if count == 1 or count == 2 then
 					meta:set_string("wireworldnext", "true")
 				end
-			elseif wireworld == 1 or wireworld == 2 then
-				meta:set_string("wireworldnext", "true")
 			else
 				table.remove(v)
 			end
@@ -98,13 +116,9 @@ minetest.register_globalstep(function(dtime)
 		for k,v in pairs(wireworld_nodes) do
 			local node = minetest.get_node(v)
 			local node_def = minetest.registered_nodes[node.name]
-			local wireworld = minetest.get_item_group(node.name, "wireworld")
 			local meta = minetest.get_meta(v)
 			if meta:get_string("wireworldnext") == "true" then
-				if wireworld_go then
-					if wireworld == 4 then node_def.on_wireworld(v)
-					else minetest.set_node(v, {name = node_def.wireworld}) end
-				end
+				if wireworld_go then node_def.on_wireworld(v) end
 				meta:set_string("wireworldnext", "false")
 			end
 		end

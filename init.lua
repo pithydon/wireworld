1258bacab94b0acb4e20ec60707a8257dff5b9ed
pithydon <daniel@danielimac.local>
2016-05-29 15:51:50 -0600

    add wireworld switch

--- init.lua
+++ init.lua
@@ -1,7 +1,94 @@
 wireworld_nodes = {}
-wireworld_go = true
+
+local contains = function(table, element)
+  local elementstring = minetest.pos_to_string(element)
+  for _, value in pairs(table) do
+    local valuestring = minetest.pos_to_string(value)
+    if valuestring == elementstring then
+      return true
+    end
+  end
+  return false
+end
 
 local mese_def = minetest.registered_nodes["default:mese"]
+
+minetest.register_node("wireworld:wireworld_on", {
+	description = "Wireworld Switch",
+	tiles = {"default_mese_block.png^(default_steel_block.png^wireworld_pink.png^[makealpha:255,0,255)"},
+	paramtype = "light",
+  drawtype = "nodebox",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      {-0.5, -0.5, -0.5, 0.5, -0.1875, 0.5},
+    },
+  },
+	groups = mese_def.groups,
+	sounds = mese_def.sounds,
+	light_source = mese_def.light_source,
+	on_rightclick = function(pos, node, puncher)
+		local nodes = minetest.find_nodes_in_area({x = pos.x - 1, y = pos.y - 1, z = pos.z - 1}, {x = pos.x + 1, y = pos.y + 1, z = pos.z + 1}, {"group:wireworld"})
+    for i,v in ipairs(nodes) do
+      local meta = minetest.get_meta(v)
+      meta:set_string("wireworld", "stop")
+    end
+		for i,v in ipairs(nodes) do
+			local find = minetest.find_nodes_in_area({x = v.x - 1, y = v.y - 1, z = v.z - 1}, {x = v.x + 1, y = v.y + 1, z = v.z + 1}, {"group:wireworld"})
+			for i,v in ipairs(find) do
+        if not contains(nodes, v) then
+				  local meta = minetest.get_meta(v)
+          meta:set_string("wireworld", "stop")
+				  nodes[#nodes+1] = v
+        end
+			end
+		end
+    minetest.swap_node(pos, {name = "wireworld:wireworld_off"})
+	end,
+})
+
+minetest.register_node("wireworld:wireworld_off", {
+	description = "Wireworld Switch",
+  tiles = {"default_mese_block.png^[colorize:green:127^(default_steel_block.png^wireworld_pink.png^[makealpha:255,0,255)"},
+	paramtype = "light",
+  drawtype = "nodebox",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      {-0.5, -0.5, -0.5, 0.5, -0.1875, 0.5},
+    },
+  },
+	paramtype = "light",
+	groups = mese_def.groups,
+	sounds = mese_def.sounds,
+	light_source = mese_def.light_source,
+  drop = "wireworld:wireworld_on",
+	on_rightclick = function(pos, node, puncher)
+    local nodes = minetest.find_nodes_in_area({x = pos.x - 1, y = pos.y - 1, z = pos.z - 1}, {x = pos.x + 1, y = pos.y + 1, z = pos.z + 1}, {"group:wireworld"})
+    for i,v in ipairs(nodes) do
+      local meta = minetest.get_meta(v)
+      meta:set_string("wireworld", "nil")
+    end
+		for i,v in ipairs(nodes) do
+			local find = minetest.find_nodes_in_area({x = v.x - 1, y = v.y - 1, z = v.z - 1}, {x = v.x + 1, y = v.y + 1, z = v.z + 1}, {"group:wireworld"})
+			for i,v in ipairs(find) do
+        if not contains(nodes, v) then
+				  local meta = minetest.get_meta(v)
+          meta:set_string("wireworld", "nil")
+				  nodes[#nodes+1] = v
+        end
+			end
+		end
+    minetest.swap_node(pos, {name = "wireworld:wireworld_on"})
+	end,
+})
+
+minetest.register_craft({
+  output = "wireworld:wireworld_on",
+  recipe = {
+    {"default:steel_ingot","default:mese","default:steel_ingot"},
+  }
+})
 
 minetest.register_node("wireworld:mese_head", {
 	description = mese_def.description.." Head",
@@ -10,6 +97,7 @@ minetest.register_node("wireworld:mese_head", {
 	groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1, wireworldhead = 1},
 	sounds = mese_def.sounds,
 	light_source = mese_def.light_source,
+  drop = "default:mese",
 	on_wireworld = function(pos)
 		minetest.swap_node(pos, {name = "wireworld:mese_tail"})
 	end,
@@ -30,6 +118,7 @@ minetest.register_node("wireworld:mese_tail", {
 	groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1},
 	sounds = mese_def.sounds,
 	light_source = mese_def.light_source,
+  drop = "default:mese",
 	on_wireworld = function(pos)
 		minetest.swap_node(pos, {name = "default:mese"})
 	end,
@@ -73,98 +162,100 @@ if (minetest.get_modpath("mesecons_button")) then
 end
 
 if (minetest.get_modpath("mesecons_extrawires")) then
-minetest.register_node("wireworld:mese_head_powered", {
-	description = mese_def.description.." Head",
-	tiles = {"default_mese_block.png^[colorize:blue:127"},
-	paramtype = "light",
-	groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1, wireworldhead = 1},
-	sounds = mese_def.sounds,
-	light_source = 5,
-	mesecons = {conductor = {
-		state = mesecon.state.on,
-		offstate = "wireworld:mese_head",
-		rules = mesewire_rules
-	}},
-	on_wireworld = function(pos)
-		minetest.swap_node(pos, {name = "wireworld:mese_tail_powered"})
-	end,
-	on_punch = function(pos, node, puncher)
-		if puncher:get_wielded_item():get_name() == "default:torch" then
+	minetest.register_node("wireworld:mese_head_powered", {
+		description = mese_def.description.." Head",
+		tiles = {"default_mese_block.png^[colorize:blue:127"},
+		paramtype = "light",
+		groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1, wireworldhead = 1},
+		sounds = mese_def.sounds,
+		light_source = 5,
+    drop = "default:mese",
+		mesecons = {conductor = {
+			state = mesecon.state.on,
+			offstate = "wireworld:mese_head",
+			rules = mesewire_rules
+		}},
+		on_wireworld = function(pos)
 			minetest.swap_node(pos, {name = "wireworld:mese_tail_powered"})
-		end
-	end,
-	after_place_node = function(pos)
-		table.insert(wireworld_nodes, pos)
-	end,
-})
+		end,
+		on_punch = function(pos, node, puncher)
+			if puncher:get_wielded_item():get_name() == "default:torch" then
+				minetest.swap_node(pos, {name = "wireworld:mese_tail_powered"})
+			end
+		end,
+		after_place_node = function(pos)
+			table.insert(wireworld_nodes, pos)
+		end,
+	})
 
-minetest.register_node("wireworld:mese_tail_powered", {
-	description = mese_def.description.." Tail",
-	tiles = {"default_mese_block.png^[colorize:red:127"},
-	paramtype = "light",
-	groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1},
-	sounds = mese_def.sounds,
-	light_source = 5,
-	mesecons = {conductor = {
-		state = mesecon.state.on,
-		offstate = "wireworld:mese_tail",
-		rules = mesewire_rules
-	}},
-	on_wireworld = function(pos)
-		minetest.swap_node(pos, {name = "mesecons_extrawires:mese_powered"})
-	end,
-	on_punch = function(pos, node, puncher)
-		if puncher:get_wielded_item():get_name() == "default:torch" then
+	minetest.register_node("wireworld:mese_tail_powered", {
+		description = mese_def.description.." Tail",
+		tiles = {"default_mese_block.png^[colorize:red:127"},
+		paramtype = "light",
+		groups = {cracky = 1, level = 2, not_in_creative_inventory=1, wireworld = 1},
+		sounds = mese_def.sounds,
+		light_source = 5,
+    drop = "default:mese",
+		mesecons = {conductor = {
+			state = mesecon.state.on,
+			offstate = "wireworld:mese_tail",
+			rules = mesewire_rules
+		}},
+		on_wireworld = function(pos)
 			minetest.swap_node(pos, {name = "mesecons_extrawires:mese_powered"})
-		end
-	end,
-	after_place_node = function(pos)
-		table.insert(wireworld_nodes, pos)
-	end,
-})
+		end,
+		on_punch = function(pos, node, puncher)
+			if puncher:get_wielded_item():get_name() == "default:torch" then
+				minetest.swap_node(pos, {name = "mesecons_extrawires:mese_powered"})
+			end
+		end,
+		after_place_node = function(pos)
+			table.insert(wireworld_nodes, pos)
+		end,
+	})
 
-minetest.override_item("mesecons_extrawires:mese_powered", {
-	groups = {cracky = 1, level = 2, wireworld = 2},
-	on_wireworld = function(pos)
-		minetest.swap_node(pos, {name = "wireworld:mese_head_powered"})
-	end,
-	on_punch = function(pos, node, puncher)
-		if puncher:get_wielded_item():get_name() == "default:torch" then
+	minetest.override_item("mesecons_extrawires:mese_powered", {
+		groups = {cracky = 1, level = 2, wireworld = 2},
+		on_wireworld = function(pos)
 			minetest.swap_node(pos, {name = "wireworld:mese_head_powered"})
-		end
-	end,
-	after_place_node = function(pos)
-		table.insert(wireworld_nodes, pos)
-	end,
-})
+		end,
+		on_punch = function(pos, node, puncher)
+			if puncher:get_wielded_item():get_name() == "default:torch" then
+				minetest.swap_node(pos, {name = "wireworld:mese_head_powered"})
+			end
+		end,
+		after_place_node = function(pos)
+			table.insert(wireworld_nodes, pos)
+		end,
+	})
 
-minetest.override_item("wireworld:mese_head", {
-	mesecons = {conductor = {
-		state = mesecon.state.off,
-		onstate = "wireworld:mese_head_powered",
-		rules = mesewire_rules
-	}}
-})
+	minetest.override_item("wireworld:mese_head", {
+		mesecons = {conductor = {
+			state = mesecon.state.off,
+			onstate = "wireworld:mese_head_powered",
+			rules = mesewire_rules
+		}}
+	})
 
-minetest.override_item("wireworld:mese_tail", {
-	mesecons = {conductor = {
-		state = mesecon.state.off,
-		onstate = "wireworld:mese_tail_powered",
-		rules = mesewire_rules
-	}}
-})
+	minetest.override_item("wireworld:mese_tail", {
+		mesecons = {conductor = {
+			state = mesecon.state.off,
+			onstate = "wireworld:mese_tail_powered",
+			rules = mesewire_rules
+		}}
+	})
 end
 
 if (minetest.get_modpath("tnt")) then
-minetest.override_item("tnt:tnt", {
-	groups = {dig_immediate = 2, mesecon = 2, tnt = 1, wireworld = 2},
-	on_wireworld = function(pos)
-		minetest.set_node(pos, {name = "tnt:tnt_burning"})
-	end,
-	after_place_node = function(pos)
-		table.insert(wireworld_nodes, pos)
-	end,
-})
+	minetest.override_item("tnt:tnt", {
+		groups = {dig_immediate = 2, mesecon = 2, tnt = 1, wireworld = 2},
+		on_wireworld = function(pos)
+			minetest.set_node(pos, {name = "tnt:tnt_burning"})
+		end,
+		after_place_node = function(pos)
+			table.insert(wireworld_nodes, pos)
+		end,
+	})
 end
 
 local timer = 0
@@ -172,20 +263,22 @@ minetest.register_globalstep(function(dtime)
 	timer = timer + dtime;
 	if timer >= 0.1 then
 		for k,v in pairs(wireworld_nodes) do
-			local node = minetest.get_node(v)
-			local wireworld = minetest.get_item_group(node.name, "wireworld")
 			local meta = minetest.get_meta(v)
-			if wireworld == 1 then
-				meta:set_string("wireworldnext", "true")
-			elseif wireworld == 2 then
-				local table = minetest.find_nodes_in_area({x = v.x - 1, y = v.y - 1, z = v.z - 1}, {x = v.x + 1, y = v.y + 1, z = v.z + 1}, {"group:wireworldhead"})
-				local count = 0
-				for _ in pairs(table) do count = count + 1 end
-				if count == 1 or count == 2 then
-					meta:set_string("wireworldnext", "true")
+			if meta:get_string("wireworld") ~= "stop" then
+				local node = minetest.get_node(v)
+				local wireworld = minetest.get_item_group(node.name, "wireworld")
+				if wireworld == 1 then
+					meta:set_string("wireworld", "next")
+				elseif wireworld == 2 then
+					local table = minetest.find_nodes_in_area({x = v.x - 1, y = v.y - 1, z = v.z - 1}, {x = v.x + 1, y = v.y + 1, z = v.z + 1}, {"group:wireworldhead"})
+					local count = 0
+					for _ in pairs(table) do count = count + 1 end
+					if count == 1 or count == 2 then
+						meta:set_string("wireworld", "next")
+					end
+				else
+					table.remove(v)
 				end
-			else
-				table.remove(v)
 			end
 		end
 		timer = 0
@@ -197,42 +290,17 @@ minetest.register_globalstep(function(dtime)
 	timer2 = timer2 + dtime;
 	if timer2 >= 0.1 then
 		for k,v in pairs(wireworld_nodes) do
-			local node = minetest.get_node(v)
-			local node_def = minetest.registered_nodes[node.name]
 			local meta = minetest.get_meta(v)
-			if meta:get_string("wireworldnext") == "true" then
-				if wireworld_go then node_def.on_wireworld(v) end
-				meta:set_string("wireworldnext", "false")
+			if meta:get_string("wireworld") == "next" then
+				local node = minetest.get_node(v)
+				local node_def = minetest.registered_nodes[node.name]
+				node_def.on_wireworld(v)
+				meta:set_string("wireworld", "nil")
 			end
 		end
 		timer2 = 0
 	end
 end)
-
-minetest.register_privilege("wireworld", {
-	description = "Allows player to start and stop wireworld.",
-	give_to_singleplayer = false,
-})
-
-minetest.register_chatcommand("wwstart", {
-	params = "",
-	description = "Start Wireworld",
-	privs = {wireworld = true},
-	func = function()
-		wireworld_go = true
-		return true
-	end,
-})
-
-minetest.register_chatcommand("wwstop", {
-	params = "",
-	description = "Stop Wireworld",
-	privs = {wireworld = true},
-	func = function()
-		wireworld_go = false
-		return true
-	end,
-})
 
 minetest.register_lbm({
 	name = "wireworld:register_nodes",
